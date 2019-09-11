212d91563e1a3525da1a03bb4cd6086e5e1fdd34
Felipe Martins <fe.mmo515@gmail.com>
2019-09-11 10:41:13 -0300

    Criacao do projeto

---
+++ src/main/java/com/felipe/cubefm/CubefmApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.cubefm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubefmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubefmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipe/cubefm/CubefmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.cubefm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubefmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
