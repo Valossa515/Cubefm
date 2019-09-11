8d37516efd91f8264df745ba9a563f98c500fb5e
Felipe Martins <fe.mmo515@gmail.com>
2019-09-11 11:24:51 -0300

    Testando a primeira classe de dominio categoria

--- src/main/java/com/felipe/cubefm/resources/CategoriaResource.java
+++ src/main/java/com/felipe/cubefm/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.felipe.cubefm.resources;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.felipe.cubefm.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categoria")
+@RequestMapping(value="/categorias")
 public class CategoriaResource
 {
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar()
+	public List<Categoria> listar()
 	{
-		return "REST está funcionando";
+		Categoria cat1 = new Categoria(1, "Pluzzles");
+		Categoria cat2 = new Categoria(1, "Card Games");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
