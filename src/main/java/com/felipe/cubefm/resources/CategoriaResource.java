576bc83a84a6ebe6266e5905669c1c827db17edb
Felipe Martins <fe.mmo515@gmail.com>
2019-09-11 12:55:48 -0300

    criando repository, resource e service de categoria

--- src/main/java/com/felipe/cubefm/resources/CategoriaResource.java
+++ src/main/java/com/felipe/cubefm/resources/CategoriaResource.java
@@ -1,26 +1,25 @@
 package com.felipe.cubefm.resources;
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipe.cubefm.domain.Categoria;
+import com.felipe.cubefm.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource
 {
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar()
+	@Autowired
+	CategoriaService service;
+	@RequestMapping(value ="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Categoria> find(@PathVariable Integer id)
 	{
-		Categoria cat1 = new Categoria(1, "Pluzzles");
-		Categoria cat2 = new Categoria(1, "Card Games");
+		Categoria obj = service.buscar(id);
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		return lista;
+		return ResponseEntity.ok().body(obj);
 	}
 }
