993a7aa5f0b3eae8924f1cd375f9acd18311168b
Felipe Martins <fe.mmo515@gmail.com>
2019-09-18 16:16:15 -0300

    Listando todas as categorias com CategoriaDTO

--- src/main/java/com/felipe/cubefm/resources/CategoriaResource.java
+++ src/main/java/com/felipe/cubefm/resources/CategoriaResource.java
@@ -1,5 +1,7 @@
 package com.felipe.cubefm.resources;
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felipe.cubefm.domain.Categoria;
+import com.felipe.cubefm.dto.CategoriaDTO;
 import com.felipe.cubefm.services.CategoriaService;
 
 @RestController
@@ -44,5 +47,11 @@ public class CategoriaResource
 	{
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> dto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(dto);
 	}
 }
