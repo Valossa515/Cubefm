7f97610d7789c706d87fe054d759f426be61f40b
Felipe Martins <fe.mmo515@gmail.com>
2020-06-01 18:11:45 -0300

    validacao customizada, emailnão repetido na inserção de cliente

--- src/main/java/com/felipe/cubefm/repositories/ClienteRepository.java
+++ src/main/java/com/felipe/cubefm/repositories/ClienteRepository.java
@@ -2,13 +2,15 @@ package com.felipe.cubefm.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.stereotype.Repository;
 
-import com.felipe.cubefm.domain.Categoria;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import com.felipe.cubefm.domain.Cliente;
 
 @Repository 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	
-
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
+	
 }
--- src/main/java/com/felipe/cubefm/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/felipe/cubefm/services/validation/ClienteInsertValidator.java
@@ -3,13 +3,20 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.felipe.cubefm.resources.exceptionhandler.FieldMessage;
 import com.felipe.cubefm.services.validation.utils.BR;
+import com.felipe.cubefm.domain.Cliente;
 import com.felipe.cubefm.domain.enums.TipoCliente;
 import com.felipe.cubefm.dto.ClienteNewDTO;
+import com.felipe.cubefm.repositories.ClienteRepository;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository clienteReporitory;
 	@Override
 	public void initialize(ClienteInsert ann) {
 
@@ -26,6 +33,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj()))
 		{
 		  list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = clienteReporitory.findByEmail(objDto.getEmail());
+		
+		if(aux != null)
+		{
+			list.add(new FieldMessage("email", "Email já existente!!!"));
 		}
 		
 		for (FieldMessage e : list) {
