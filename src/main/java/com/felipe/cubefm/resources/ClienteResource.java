8b8655c468714fcdd5515338ce02dd6130cff57c
Felipe Martins <fe.mmo515@gmail.com>
2020-06-01 12:44:41 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/felipe/cubefm/resources/ClienteResource.java
+++ src/main/java/com/felipe/cubefm/resources/ClienteResource.java
@@ -1,4 +1,5 @@
 package com.felipe.cubefm.resources;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felipe.cubefm.domain.Cliente;
 import com.felipe.cubefm.dto.ClienteDTO;
+import com.felipe.cubefm.dto.ClienteNewDTO;
 import com.felipe.cubefm.services.ClienteService;
 
 @RestController
@@ -28,6 +31,15 @@ public class ClienteResource
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto)
+	{
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
@@ -57,7 +69,6 @@ public class ClienteResource
 			@RequestParam(value="orderBy", defaultValue="nome") String orderBy,
 			@RequestParam(value="direction", defaultValue="ASC") String direction) {
 		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
-		
 		// "Page" já é Java 8 compliance, então não é necessário o uso do stream e do collect
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDto);
