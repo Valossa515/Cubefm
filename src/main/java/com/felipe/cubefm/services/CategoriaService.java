fcec4556b2e2ced64c88b838f2f8b719c0577180
Felipe Martins <fe.mmo515@gmail.com>
2020-06-01 11:47:51 -0300

    Correcoes: updaate em CategoriaService e construtor de Pagamento

--- src/main/java/com/felipe/cubefm/services/CategoriaService.java
+++ src/main/java/com/felipe/cubefm/services/CategoriaService.java
@@ -36,8 +36,9 @@ public class CategoriaService
 	
 	public Categoria update(Categoria obj) 
 	{
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	public void delete(Integer id) 
 	{
@@ -69,5 +70,4 @@ public class CategoriaService
 	{
 		newObj.setNome(obj.getNome());
 	}
-	
 }
