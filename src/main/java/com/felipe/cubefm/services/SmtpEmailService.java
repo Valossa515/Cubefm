c294a8bb6fc89dd80a6821b38c8e0970184f44e7
Felipe Martins <fe.mmo515@gmail.com>
2020-06-17 13:48:27 -0300

    Email HTML

--- src/main/java/com/felipe/cubefm/services/SmtpEmailService.java
+++ src/main/java/com/felipe/cubefm/services/SmtpEmailService.java
@@ -1,15 +1,19 @@
 package com.felipe.cubefm.services;
 
+import javax.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractMailService{
 	
 	@Autowired
 	private MailSender mailSender;
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 	@Override
@@ -19,6 +23,11 @@ public class SmtpEmailService extends AbstractMailService{
 		LOG.info("Email enviado!!!");
 		
 	}
-	
-
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando...");
+		javaMailSender.send(msg);
+		LOG.info("Email enviado!!!");
+		
+	}
 }
