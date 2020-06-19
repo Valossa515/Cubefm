bc2d57f206cb54433e6b43e2c7c7a1c3b1b0f7d3
Felipe Martins <fe.mmo515@gmail.com>
2020-06-19 11:20:42 -0300

    Esqueci a senha

--- src/main/java/com/felipe/cubefm/services/AbstractMailService.java
+++ src/main/java/com/felipe/cubefm/services/AbstractMailService.java
@@ -14,6 +14,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.felipe.cubefm.domain.Cliente;
 import com.felipe.cubefm.domain.Pedido;
 
 
@@ -70,5 +71,22 @@ public abstract class AbstractMailService implements EmailService {
 		mmh.setSentDate(new Date(System.currentTimeMillis()));
 		mmh.setText(htmlFromTemplatePedido(obj), true);
 		return mimeMessage;
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass)
+	{
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha: ");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
+		return sm;
 	}
 }
