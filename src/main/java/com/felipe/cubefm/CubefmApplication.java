dbc9f54d35274e8e0a193bdfa63e4f36a365f5cc
Felipe Martins <fe.mmo515@gmail.com>
2020-06-24 17:08:30 -0300

    Enviando Imagen via endpoint

--- src/main/java/com/felipe/cubefm/CubefmApplication.java
+++ src/main/java/com/felipe/cubefm/CubefmApplication.java
@@ -1,22 +1,17 @@
 package com.felipe.cubefm;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.felipe.cubefm.services.S3Service;
-
 @SpringBootApplication
 public class CubefmApplication implements CommandLineRunner {
-	@Autowired
-	private S3Service s3Service;
 	public static void main(String[] args) {
 		SpringApplication.run(CubefmApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\temp\\Minions.jpg");
+		
 	}
 }
--- src/main/java/com/felipe/cubefm/services/ClienteService.java
+++ src/main/java/com/felipe/cubefm/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.felipe.cubefm.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.felipe.cubefm.domain.Cidade;
 import com.felipe.cubefm.domain.Cliente;
@@ -36,6 +38,8 @@ public class ClienteService
 	private EnderecoRepository enderecoRepository;
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -109,6 +113,11 @@ public class ClienteService
 	{
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile)
+	{
+		return s3Service.uploadFile(multipartFile);
 	}
 	
 }
